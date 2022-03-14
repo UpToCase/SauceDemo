4358843be97022dc63faad64c7377707e105849e
UpToCase <99139170+UpToCase@users.noreply.github.com>
2022-03-14 12:03:47 +0100

    create LoginPage

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+    public static final By USERNAME_INPUT = By.id("user-name");
+    public static final By PASSWORD_INPUT = By.id("password");
+    public static final By LOGIN_BUTTON = By.id("login-button");
+    public static final By ERROR_MESSAGES = By.cssSelector("[data-test=error]");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void openPage() {
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    public void login(String user, String password) {
+        openPage();
+        driver.findElement(USERNAME_INPUT).sendKeys(user);
+        driver.findElement(PASSWORD_INPUT).sendKeys(password);
+        driver.findElement(LOGIN_BUTTON).click();
+    }
+
+    public String getError() {
+        return driver.findElement(ERROR_MESSAGES).getText();
+    }
+}
